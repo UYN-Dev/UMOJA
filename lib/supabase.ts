e66d258671ea7ea8a3bd2fcfda7c5e370ed7f957
samikshagupta26 <gsamiksha2003@gmail.com>
2025-11-11 01:49:55 -0600

    Fix Supabase integration; enable saving child profile

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://rhtonsoheuohpqqmuyaj.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJodG9uc29oZXVvaHBxcW11eWFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0MDA5NDMsImV4cCI6MjA2NTk3Njk0M30.BYzXKV9lAV0zeFjWTbtEw74g6RQ03ZmqdqiT4PUIBU4'
-
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 export const supabase = createClient(supabaseUrl, supabaseKey) 
